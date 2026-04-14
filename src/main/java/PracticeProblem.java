45ab7e7244982cb284066d80c7d93c5be924426f
aA-Set-sun <343303426@tdsb.ca>
2026-04-14 15:40:14 +0000

    that's n ot a pyramid

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -2,8 +2,42 @@ public class PracticeProblem {
 
 	public static void main(String args[]) {
 
+		System.out.println(triangle(3));
+		System.out.println(pyramid(4));
+
 	}
 
-	
+	public static String triangle (int num) {
+		String result = "";
 
+		for (int i = 0; i < num; i++) {
+			System.out.println(result); 
+			result = result + '*';			
+		}
+		return result;
+	}
+
+	public static String pyramid (int num) {
+		String result = "";
+		String spaces = " ";
+		int space = 0;
+
+		for (int i = 0; i < num; i++) {
+			for (int n = i; n < num; n++) {
+				space++;
+				while spaces 
+				spaces = spaces + space;
+				spaces = spaces.replaceAll(spaces, " ");
+
+			}
+			System.out.println(result);
+			
+			result = spaces + result + " " + '*';		
+		}
+		return result;
+
+			
+	}
+	
+		
 }
